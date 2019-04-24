d18df3268a33039f4ad4a0213d5be16fc0d1b7f5
Eduardo Godoy <eduardo.fuso@novaandradina.org>
2019-04-24 10:39:15 -0300

    Alterando inputs e ajustes

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -14,27 +14,28 @@
   var counter = t;
 
   document.getElementById("form").addEventListener("submit",(e)=>{
-    var motorista = document.getElementById("motorista").value;
+    // var motorista = document.getElementById("motorista").value;
     var veiculo = document.getElementById("veiculo").value;
     var placa = document.getElementById("placa").value;
     var km = document.getElementById("km").value;
+    var kmFinal = document.getElementById("kmFinal").value;
     var finalidade = document.getElementById("finalidade").value;
     var dtSaida = document.getElementById("dtSaida").value;
     var hrSaida = document.getElementById("hrSaida").value;
     e.preventDefault();
-    salvarViagem(motorista, veiculo, placa, km, finalidade, dtSaida, hrSaida);
+    salvarViagem(veiculo, placa, km, kmFinal, finalidade, dtSaida, hrSaida);
     form.reset();
   });
 
-  function salvarViagem(motorista, veiculo, placa, km, finalidade, dtSaida, hrSaida){
+  function salvarViagem(veiculo, placa, km, kmFinal, finalidade, dtSaida, hrSaida){
       console.log(counter); // Retirar da fase de testes
       counter+=1;
       var viagem={
           id:counter,
-          motorista:motorista,
           veiculo:veiculo,
           placa:placa,
           km:km,
+          kmFinal:kmFinal,
           finalidade:finalidade,
           dtSaida:dtSaida,
           hrSaida:hrSaida
