d9d4ffe9fc3241aa0e6574f8b295479807c825ba
anacarolynam <ana.nunes2@estudante.ifms.edu.br>
2019-05-10 17:12:34 -0400

    Listando placas e incrementando auto complete

--- public/js/selectVeiculo.js
+++ public/js/selectVeiculo.js
@@ -8,4 +8,36 @@ const listaVeiculos = () => {
 
 }
 
+
+const buscaKm = () => {
+    console.log(`Entrou no buscaKM`);
+    
+    let placa = document.getElementById('placa').value
+
+    firebase.database().ref('veiculos/').on('value', function(snapshot) {
+        for (let i = 1; i < snapshot.val().length; i++) {
+            if (snapshot.val()[i].placaVeiculo == placa) {
+                console.log(snapshot.val()[i].kmVeiculo);
+                
+                document.getElementById('km').value = snapshot.val()[i].kmVeiculo
+            }
+        }
+    })
+}
+
+
+const buscaplaca = () => {
+    let nomeVeiculo = document.getElementById('veiculo').value
+
+    firebase.database().ref('veiculos/').on('value', function(snapshot) {
+        for (let i = 1; i < snapshot.val().length; i++) {
+            if (snapshot.val()[i].nomeVeiculo == nomeVeiculo) {
+                document.getElementById('placa').innerHTML = document.getElementById('placa').innerHTML +
+                `<option value="${snapshot.val()[i].placaVeiculo}">${snapshot.val()[i].placaVeiculo}</option>`
+            }
+        }
+    })
+
+}
+
 listaVeiculos()
